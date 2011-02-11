3af5b1a700f040c8297b9f3a5b201097e1f59c1b
sergio bossa <sergio.bossa@gmail.com>
2011-02-11 22:46:08 +0100

    Imports cleanup.

--- src/test/java/terrastore/client/test/integration/TerrastoreClientIntegrationTest.java
+++ src/test/java/terrastore/client/test/integration/TerrastoreClientIntegrationTest.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
